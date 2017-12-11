453372645b6c1fbc8cb8a10e205c61534a1f7dc4
Gustave Mugabo <gustave.mugabo@cgius.com>
2017-12-10 21:04:46 -0500

    Initial properties  configuration and code wareframe

---
+++ inventory-management-service/src/main/java/com/kiravita/inventorymanagementservice/configuration/InventoryManagementServiceConfiguration.java
@@ -0,0 +1,13 @@
+package com.kiravita.inventorymanagementservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="inventory-ms")
+public class InventoryManagementServiceConfiguration {
+	
+	
+	
+
+}
