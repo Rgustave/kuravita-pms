ef7269f64445aee6c7076061bc4c14100da55661
Gustave Mugabo <mugustave@gmail.com>
2018-01-03 22:24:33 -0500

    code refactoring: CurrentInventory and Category

--- inventory-management-service/src/main/java/com/kuravita/inventorymanagementservice/model/CurrentInventory.java
+++ inventory-management-service/src/main/java/com/kuravita/inventorymanagementservice/model/CurrentInventory.java
@@ -1,159 +1,69 @@
 package com.kuravita.inventorymanagementservice.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
+import lombok.Data;
 
 /*
  * This class models a store specific product. 
     @author blessed.mununuzi
  */
-
+@Data
 @Entity
-@Table(name="current_inventory")
-public class CurrentInventory {
+@Table(name = "current_inventory")
+public class CurrentInventory implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 316318072113966424L;
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="current_inventory_id")
-	@JsonProperty("CURRENT_INVENTORY_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column()
 	private Long currentInventoryId;
-	
-	@Column(name = "upc", unique = true, nullable = false, length = 45)
-	@JsonProperty("UPC")
+
+	@Column(unique = true, nullable = false, length = 45)
 	private String upc;
-	
-	@Column(name="checked_in_dt_tm")
-	@JsonProperty("CHECKED_IN_DT_TM")
+
+	@Column()
 	private Date checkedInDtTm;
-	
-	@Column(name="checked_out_dt_tm")
-	@JsonProperty("CHECKED_OUT_DT_TM")
+
+	@Column()
 	private Date checkedOutDtTm;
-	
-	@Column(name="expiration_dt_tm")
-	@JsonProperty("EXPIRATION_DT_TM")
+
+	@Column()
 	private Date expirationDtTm;
-	
-	@JsonProperty("QUANTITY")
+
+	@Column()
 	private Integer quantity;
-	
-	@Column(name="default_packaging", length = 45)
-	@JsonProperty("DEFAULTPACKAGING")
+
+	@Column(length = 45)
 	private String defaultPackaging;
-	
-	@Column(name="smallest_packaging", length = 45)
-	@JsonProperty("SMALLEST_PACKAGING")
+
+	@Column( length = 45)
 	private String smallestPackaging;
-	
-	@JoinColumn(name="packageTypeId")
-	@JsonProperty("PACKAGING_TYPE_ID")
-	private Package packageType;
-	
-	@JoinColumn(name="productId")
-	@JsonProperty("PRODUCT_ID")
-	private Product product ;
-	
-	@JoinColumn(name="storeId")
-	@JsonProperty("STORE_ID")
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "package_type_id")
+	private PackageType packageType;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
 	private Store store;
-
-	public Long getCurrentInventoryId() {
-		return currentInventoryId;
-	}
-
-	public void setCurrentInventoryId(Long currentInventoryId) {
-		this.currentInventoryId = currentInventoryId;
-	}
-
-	public String getUpc() {
-		return upc;
-	}
-
-	public void setUpc(String upc) {
-		this.upc = upc;
-	}
-
-	public Date getCheckedInDtTm() {
-		return checkedInDtTm;
-	}
-
-	public void setCheckedInDtTm(Date checkedInDtTm) {
-		this.checkedInDtTm = checkedInDtTm;
-	}
-
-	public Date getCheckedOutDtTm() {
-		return checkedOutDtTm;
-	}
-
-	public void setCheckedOutDtTm(Date checkedOutDtTm) {
-		this.checkedOutDtTm = checkedOutDtTm;
-	}
-
-	public Date getExpirationDtTm() {
-		return expirationDtTm;
-	}
-
-	public void setExpirationDtTm(Date expirationDtTm) {
-		this.expirationDtTm = expirationDtTm;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getDefaultPackaging() {
-		return defaultPackaging;
-	}
-
-	public void setDefaultPackaging(String defaultPackaging) {
-		this.defaultPackaging = defaultPackaging;
-	}
-
-	public String getSmallestPackaging() {
-		return smallestPackaging;
-	}
-
-	public void setSmallestPackaging(String smallestPackaging) {
-		this.smallestPackaging = smallestPackaging;
-	}
-
-	public Package getPackageType() {
-		return packageType;
-	}
-
-	public void setPackageType(Package packageType) {
-		this.packageType = packageType;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public Store getStore() {
-		return store;
-	}
-
-	public void setStore(Store store) {
-		this.store = store;
-	}
-
-	
 
 }
